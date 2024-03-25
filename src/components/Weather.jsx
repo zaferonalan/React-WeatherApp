8dee5e971f2aa7abfa0df5287bea5e658a46d275
zafergunayonalan <zafergunay7@gmail.com>
2024-03-25 23:58:25 +0300

    dataChecked

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,23 +1,25 @@
-import React from "react";
-
-const Weather = ({setLocation,location}) => {
-
-  const handleLocationChange = (event) => {
-    setLocation(event.target.value)
-  }
-
+import axios from "axios";
+import React, { useState } from "react";
+const Weather = () => {
+const [city, setCity] = useState("")
+// console.log(city)
+const handleChange = () => 
+{
+  const api = "2e14d9a37a744faa77f7d49e092a7524"
+  const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metic&lang=tr`
+  axios(baseURL).then(data => console.log(data))
+}
   return (
     <div className="app-container">
       <h1 className="app-title">Hava Durumu Uygulaması</h1>
-      <div className="input-container">
-        <input
-          className="location-input"
-          type="text"
-          placeholder="Şehir Giriniz"
-          value={location}
-          onChange={handleLocationChange}
-        />
-      </div>
+      <form onSubmit={(e)=>{e.preventDefault(); handleChange()}}>
+        <div className="form" >
+          <input onChange={(e)=>{setCity(e.target.value)}} className="inputText" placeholder="Şehri Giriniz" type="text" />
+        </div>
+        <div className="btnDiv">
+          <button type="submit" className="btn" >Verileri Getir</button>
+        </div>
+      </form>
     </div>
   );
 };
